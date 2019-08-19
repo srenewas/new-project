6d3312991ea04994cb80bda40fac2b3b902188ec
SEENU IGLESIAS <SEENU IGLESIAS@DESKTOP-NM4PMIF>
2019-08-19 12:50:45 +0530

    NEW COMMIT

--- NEW TEST/src/com/seenu/test.java
+++ NEW TEST/src/com/seenu/test.java
@@ -4,13 +4,6 @@ import java.util.*;
 
 public class test {
 	public static void main(String[] args) {
-		String in = "SEENU IGLESIAS";
-		char tocheck = 'E';
-		int count = 0;
-		for(int i = 0;i<in.length();i++) {
-			if(in.charAt(i)==tocheck)
-				count++;
-		}
-		System.out.println(count);
-		}
+		System.out.println("WELCOME TO GIT WORLD");
+	}
 	}
